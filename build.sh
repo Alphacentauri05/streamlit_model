0787ff7aeb5043d0d1a7d523e0c88d0635b10ad2
Alphacentauri05 <udaysharma051203@gmail.com>
2025-03-22 00:59:22 +0530

    Added pyaudio

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update package lists
-apt-get update 
-
-# Install PortAudio and required dependencies
-apt-get install -y portaudio19-dev python3-pyaudio
-
-# Install required Python packages
-pip install --no-cache-dir -r requirements.txt
